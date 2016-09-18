976e4a7acdc672387c8b168c77e5875b48088c70
翁鹏乐 <286699511@qq.com>
2016-09-18 19:59:28 +0800

    版本1.1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,13 +8,13 @@
 </head>
 
 <body>
-<iframe width="100%" height="90px" name="main" scrolling="auto" frameborder="0" src="top.blade.php" style="position:fixed;top:0; left:0"></iframe>
+<iframe width="100%" height="90px" name="main" scrolling="auto" frameborder="0" src="{{url('admin/top')}}" style="position:fixed;top:0; left:0"></iframe>
 <div class="" style=" padding-top:90px">
 	<div class="" style="width:18%; float:left; height:1150px;">
-		<iframe src="left.blade.php" width="100%" height="100%" name="leftFrame" id="leftFrame" scrolling="No" title="leftFrame"  frameborder="0" framespacing="0" /></iframe>
+		<iframe src="{{url('admin/left')}}" width="100%" height="100%" name="leftFrame" id="leftFrame" scrolling="No" title="leftFrame"  frameborder="0" framespacing="0" /></iframe>
 	</div>
 	<div style="float:right; width:82%; height:1000px;" >
-    	<iframe src="right.blade.php" width="100%" height="100%" name="rightFrame" id="rightFrame" scrolling="No" title="rightFrame" frameborder="0" framespacing="0" /></iframe>
+    	<iframe src="{{url('admin/right')}}" width="100%" height="100%" name="rightFrame" id="rightFrame" scrolling="No" title="rightFrame" frameborder="0" framespacing="0" /></iframe>
     </div>
 </div>
 </body>
