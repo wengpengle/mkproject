f6ce559997b64d16bc2acace2ebfb49c9bbc3861
翁鹏乐 <286699511@qq.com>
2016-09-21 16:38:49 +0800

    版本2.6

--- resources/views/common/left.blade.php
+++ resources/views/common/left.blade.php
@@ -10,7 +10,7 @@
 <body>
 <div class="left_slide_nav">
   <div class="business">
-      <dl class="dl_list">
+      {{--<dl class="dl_list">
           <dt class="dl_open"><span class="expend_icon"></span><a href="javascript:;">权限管理</a></dt><!--打开状态替换close为open-->
           <dd><a href="xzgly.html" target="rightFrame">新增管理员</a></dd>
           <dd><a href="gly_list.html" target="rightFrame">管理员列表</a></dd>
@@ -19,7 +19,7 @@
           <dd><a href="xzqx.html" target="rightFrame">新增权限</a></dd>
           <dd><a href="fpqx.html" target="rightFrame">分配权限</a></dd>
           <!--当前页面导航条dl添加class为dl_height,dt添加class为dl_open,dd添加class为dd_current-->
-      </dl>
+      </dl>--}}
     <dl class="dl_list">
       <dt class="dl_open"><span class="expend_icon"></span><a href="javascript:;">课程管理</a></dt><!--打开状态替换close为open-->
       <dd><a href="{{url('admin/course')}}" target="rightFrame">新增课程</a></dd>
@@ -54,37 +54,10 @@
       <dd><a href="{{url('admin/part_lists')}}" target="rightFrame">实战课程列表</a></dd>
     </dl>
     <dl class="dl_list dl_3">
-      <dt class="dl_open"><span class="expend_icon"></span><a href="javascript:;">违纪管理</a></dt><!--打开状态替换close为open-->
-      <dd><a href="xzwj.html" target="rightFrame">新增违纪</a></dd>
-      <dd><a href="wj_list.html" target="rightFrame">违纪列表</a></dd>
-      <dd><a href="javascript:;" target="rightFrame">回收站</a></dd>
-    </dl>
-    <dl class="dl_list dl_3">
-      <dt class="dl_open"><span class="expend_icon"></span><a href="javascript:;">违纪统计</a></dt><!--打开状态替换close为open-->
-      <dd><a href="javascript:;" target="rightFrame">班级违纪排名</a></dd>
-      <dd><a href="javascript:;" target="rightFrame">阶段违纪对比</a></dd>
-      <dd><a href="javascript:;" target="rightFrame">违纪类型分析</a></dd>
-      <dd><a href="javascript:;" target="rightFrame">互查违纪个数</a></dd>
-      <dd><a href="javascript:;" target="rightFrame">讲师违纪KPI</a></dd>
-      <dd><a href="javascript:;" target="rightFrame">班主任违纪KPI</a></dd>
-    </dl>
-    <dl class="dl_list dl_3">
-      <dt class="dl_open"><span class="expend_icon"></span><a href="javascript:;">作业管理</a></dt><!--打开状态替换close为open-->
-      <dd><a href="javascript:;" target="rightFrame">新增作业</a></dd>
-      <dd><a href="javascript:;" target="rightFrame">作业记录</a></dd>
-    </dl>
-    <dl class="dl_list dl_3">
-      <dt class="dl_open"><span class="expend_icon"></span><a href="javascript:;">日志管理</a></dt><!--打开状态替换close为open-->
-      <dd><a href="javascript:;" target="rightFrame">添加日志</a></dd>
-      <dd><a href="javascript:;" target="rightFrame">我的日志</a></dd>
-      <dd><a href="xsrz_list.html" target="rightFrame">学生日志列表</a></dd>
-    </dl>
-    <dl class="dl_list dl_3">
       <dt class="dl_open"><span class="expend_icon"></span><a href="javascript:;">密码管理</a></dt><!--打开状态替换close为open-->
       <dd><a href="xgmm.html" target="rightFrame">修改密码</a></dd>
     </dl>
   </div>
 </div>
-
 </body>
 </html>
